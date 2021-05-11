663ddf8c19849bf9b54433760eeb71c5f8aef483
guppe <mst39639toi8@gmail.com>
2021-05-11 11:32:02 +0900

    change

--- app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
@@ -1,7 +1,7 @@
 package app.myoji.nickname.appjam
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,16 +11,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var number:Int = 0;
+        var number:Int = 0
 
-        var numberTextView: TextView = findViewById<TextView>(R.id.numberTextView);
-        numberTextView.text = "0";
+        var numberTextView: TextView = findViewById<TextView>(R.id.numberTextView)
+        numberTextView.text = "0"
 
-        var plusButton: Button = findViewById<Button>(R.id.plusButton);
-        plusButton.setOnClickListener {
-            number += 1;
-            numberTextView.text = number.toString();
+        var bear1: ImageButton = findViewById(R.id.bear1)
+        var bear2: ImageButton = findViewById(R.id.bear2)
+        var bear3: ImageButton = findViewById(R.id.bear3)
+        var bear4: ImageButton = findViewById(R.id.bear4)
+        var bear5: ImageButton = findViewById(R.id.bear5)
+        var bear6: ImageButton = findViewById(R.id.bear6)
+        var bear7: ImageButton = findViewById(R.id.bear7)
+        var bear8: ImageButton = findViewById(R.id.bear8)
+        var bear9: ImageButton = findViewById(R.id.bear9)
+        var bear10: ImageButton = findViewById(R.id.bear10)
+        var bear11: ImageButton = findViewById(R.id.bear11)
+        var bear12: ImageButton = findViewById(R.id.bear12)
+        var bear13: ImageButton = findViewById(R.id.bear13)
+        var bear14: ImageButton = findViewById(R.id.bear14)
+        var bear15: ImageButton = findViewById(R.id.bear15)
+
+        var bee1: ImageButton = findViewById(R.id.bee1)
+        var bee2: ImageButton = findViewById(R.id.bee2)
+        var bee3: ImageButton = findViewById(R.id.bee3)
+        var bee4: ImageButton = findViewById(R.id.bee4)
+        var bee5: ImageButton = findViewById(R.id.bee5)
+        var bee6: ImageButton = findViewById(R.id.bee6)
+        var bee7: ImageButton = findViewById(R.id.bee7)
+        var bee8: ImageButton = findViewById(R.id.bee8)
+
+        var honey1: ImageButton = findViewById(R.id.honey1)
+        var honey2: ImageButton = findViewById(R.id.honey2)
+
+        var bear = listOf(bear1, bear2, bear3, bear4, bear5, bear6, bear7, bear8, bear9, bear10, bear11, bear12, bear13, bear14, bear15)
+        for (num in 1..15) {
+            bear[num].setOnClickListener {
+                number += 100
+                numberTextView.text = number.toString()
+            }
         }
+
+
     }
 
 }
